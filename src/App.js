eda7d9cc0064bb8a4189e465d2a93a06df6f3f1a
teazy1910 <tanjaisabel.dietz@gmail.com>
2024-10-02 10:28:49 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,116 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+// const initialItems = [
+//   { id: 1, description: "Passports", quantity: 2, packed: false },
+//   { id: 2, description: "Socks", quantity: 12, packed: true },
+//   { id: 3, description: "Charger", quantity: 12, packed: false },
+//   { id: 4, description: "Macbook", quantity: 12, packed: false },
+// ];
+
+export default function App() {
+  // TODO: Um welches Element handelt es sich? Beantwortet die Frage, welcher Initialwert als State gesetzt werden muss.
+
+  //! Dieser State muss von der Form in die App verschoben werden, weil wir die relevanten Objekte nicht anzeigen können. Die Anzeige funktioniert nur den "Tree" abwärts. (Siehe React Dev-Tools - Components)
+  //? Man nennt dies auch : Lift Up State
+  //? Man tut dies wenn die Geschwister Komponenten den State benötigen, hebt man den State in die übergeordnete Component, damit die alle drauf zugreifen könenn.
+
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
+
+  function handleDeleteItem(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Logo />
+      <Form onAddItems={handleAddItems} />
+      <PackingList items={items} onDeleteItem={handleDeleteItem} />
+      <Stats />
     </div>
   );
 }
 
-export default App;
+function Logo() {
+  return <h1>🌴 Far Away 🧳</h1>;
+}
+
+function Form({ onAddItems }) {
+  const [description, setDescription] = useState("");
+  const [select, setSelect] = useState(1);
+
+  function handleSumbit(e) {
+    e.preventDefault();
+
+    // Wenn das Formular leer abgeschickt wird, returne sofort (also tue nix)
+    if (!description) return;
+
+    const newItem = { description, select, packed: false, id: Date.now() };
+    console.log(newItem);
+
+    onAddItems(newItem);
+
+    setDescription("");
+    setSelect(1);
+  }
+
+  return (
+    <form className="add-form" onSubmit={handleSumbit}>
+      <h3>What do you need for your 😍 trip?</h3>
+      <select
+        value={select}
+        onChange={(e) => setSelect(Number(e.target.value))}>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <button>Add</button>
+    </form>
+  );
+}
+function PackingList({ items, onDeleteItem }) {
+  return (
+    <div className="list">
+      <ul>
+        {items.map((item) => (
+          <Item item={item} onDeleteItem={onDeleteItem} key={item.id} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item, onDeleteItem }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.select} {item.quantity} {item.description}
+      </span>
+      <button
+        onClick={() => {
+          onDeleteItem(item.id);
+        }}>
+        ❌
+      </button>
+    </li>
+  );
+}
+
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>You have X items on your list, and you already packed X (x%)</em>
+    </footer>
+  );
+}
